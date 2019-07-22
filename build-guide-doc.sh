493354e3683a67b214bc0a471ed775dc8c9871ce
rmaicle <rmaicle@gmail.com>
2019-07-22 10:07:54 +0800

    Fix build script

--- build-guide-doc.sh
+++ build-guide-doc.sh
@@ -2,4 +2,9 @@
 
 # Script for building the doc guide
 
-../md-to-pdf/build.sh -i ./guide-doc/source.txt -td ../latex-templates -tf doc/template_doc_us.tex
+../../../tools//md-to-pdf/build.sh      \
+    -i ./guide-doc/source.txt           \
+    -td ../../dev/doc/latex-templates   \
+    -tf ./doc/template_doc_us.tex       \
+    -od ./                              \
+    -of latex-template-doc-guide
