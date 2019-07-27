40eea77cdfd26b6aac3cdc04d9e851212d7e94c9
rmaicle <rmaicle@gmail.com>
2019-07-27 19:40:17 +0800

    Change path to build.sh
    Assumes that the parent directories of this script and build.sh
    are on the same directory and depth.
    
    <some-directory>
      |
      +--latex-template
      |    ...
      |    build-guide-doc.sh
      |
      +--md-to-pdf
           ...
           build.sh

--- build-guide-doc.sh
+++ build-guide-doc.sh
@@ -2,9 +2,9 @@
 
 # Script for building the doc guide
 
-../../../tools//md-to-pdf/build.sh      \
+../md-to-pdf/build.sh                   \
     -i ./guide-doc/source.txt           \
-    -td ../../dev/doc/latex-templates   \
+    -td ../latex-templates              \
     -tf ./doc/template_doc_us.tex       \
     -od ./                              \
     -of latex-template-doc-guide
