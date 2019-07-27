0f09ed2d390eae50e24d32e43e4ec774af7e31a9
rmaicle <rmaicle@gmail.com>
2019-07-27 22:16:21 +0800

    Use new latex template for documents

--- build-guide-doc.sh
+++ build-guide-doc.sh
@@ -5,6 +5,6 @@
 ../md-to-pdf/build.sh                   \
     -i ./guide-doc/source.txt           \
     -td ../latex-templates              \
-    -tf ./doc/template_doc_us.tex       \
+    -tf ./doc/template_doc.tex          \
     -od ./                              \
     -of latex-template-doc-guide
