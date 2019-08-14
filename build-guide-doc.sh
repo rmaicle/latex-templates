77748d286b95281f685da9d7e2f7651a7c963cfd
rmaicle <rmaicle@gmail.com>
2019-08-14 11:12:11 +0800

    Add more arguments to build script

--- build-guide-doc.sh
+++ build-guide-doc.sh
@@ -2,9 +2,33 @@
 
 # Script for building the doc guide
 
+param_debug=""
+param_show_frame=""
+param_output_latex=""
+
+while [ $# -gt 0 ]; do
+    case "${1}" in
+        -debug)         param_debug="-debug"
+                        shift
+                        ;;
+        -o)             param_output_latex="-o"
+                        shift
+                        ;;
+        -showframe)     param_show_frame="-showframe"
+                        shift
+                        ;;
+        *)              break ;;
+    esac
+done
+
 ../md-to-pdf/build.sh                   \
+    ${param_debug}                      \
+    `#-softcopy`                        \
+    -papersize usletter                 \
+    ${param_show_frame}                 \
     -i ./guide-doc/source.txt           \
     -td ../latex-templates              \
     -tf ./doc/template_doc.tex          \
+    ${param_output_latex}               \
     -od ./                              \
     -of latex-template-doc-guide
