acee7d6f9002736a03ebdbbb1ed1077650143159
rmaicle <rmaicle@gmail.com>
2021-04-17 17:12:29 +0800

    Allow passing source file in build script

--- guide-doc/build.sh
+++ guide-doc/build.sh
@@ -6,6 +6,8 @@ param_debug=""
 param_show_frame=""
 param_output_latex=""
 param_output_image_generate=""
+param_source="./source.txt"
+# param_source="./source_test.txt"
 
 while [ $# -gt 0 ]; do
     case "${1}" in
@@ -21,6 +23,7 @@ while [ $# -gt 0 ]; do
         --imagex)       param_output_image_generate="-imagex"
                         shift
                         ;;
+        --source)       param_source="${2}"; shift 2 ;;
         *)              break ;;
     esac
 done
@@ -35,7 +38,7 @@ done
     ${param_show_frame}                 \
     ${param_output_image_generate}      \
     `#-copyrightx`                      \
-    -i ./source.txt                     \
+    -i "${param_source}"                \
     -tf template_doc.tex                \
     ${param_output_latex}               \
     -od ./                              \
