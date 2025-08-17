541cc43a20759e95063cbe81d681bc3d0ec96c3a
rmaicle <rmaicle@gmail.com>
2025-08-18 00:25:07 +0800

    Fix paper and font size argument handling

--- doc-guide/build.sh
+++ doc-guide/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Script for building the doc guide
+#
+# ./build.sh
+# ./build.sh --paper-size a5 --font-size 8 --template template_doc_a5.tex
+# ./build.sh --paper-size b5 --font-size 10 --template template_doc_a5.tex
 
 declare SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -24,14 +28,27 @@ declare -a PDF_ENGINES=(
 
 declare -r PAPER_A4="a4"
 declare -r PAPER_A5="a5"
-declare -r PAPER_USLETTER="usletter"
+declare -r PAPER_A5="b5"
+declare -r PAPER_LETTER="letter"
 
 declare -r DEFAULT_PAPER_SIZE=""
 
 declare -a PAPER_SIZES=(
     ${PAPER_A4}
     ${PAPER_A5}
-    ${PAPER_USLETTER}
+    ${PAPER_B5}
+    ${PAPER_LETTER}
+)
+
+declare -r DEFAULT_FONT_SIZE="10"
+declare -a -r FONT_SIZES=(
+    "6"
+    "7"
+    "8"
+    "9"
+    "${DEFAULT_FONT_SIZE}"
+    "11"
+    "12"
 )
 
 declare -r DEFAULT_TEMPLATE_FILE="template_doc.tex"
@@ -64,6 +81,8 @@ Options:
       --draft             generate draft version PDF document
       --engine            engine to use, pdflatex or xelatex; default is pdflatex
 $(printf '                          %s\n' ${PDF_ENGINES[@]})
+      --font-size n       body text font point size; default is ${DEFAULT_FONT_SIZE} pt.
+$(printf '                          %s point\n' ${FONT_SIZES[@]})
       --latex             output TeX/LaTeX file and generate PDF
       --latex-only        output TeX/LaTeX file and exit
       --markdown file     use input mardown content file
@@ -72,10 +91,11 @@ $(printf '                          %s\n' ${PDF_ENGINES[@]})
       --no-backmatter     do not generate user-supplied backmatter contents
       --of file           output filename appended with '.pdf'; the default output
                             filename is 'output'
-      --paper size        paper-size; default is usletter
+      --paper-size size   paper-size; default is usletter
                             usletter
                             a4
       --show-frame        show page margins
+      --template file     use specified template file
       --use-latest        use latest installed Pandoc version
 
 NOTE: Including source files using pp !source(...) is relative to the
@@ -89,6 +109,7 @@ declare arg_template="./${DEFAULT_TEMPLATE_FILE}"
 declare arg_engine="${DEFAULT_PDF_ENGINE}"
 declare arg_paper_size=""
 
+declare param_font_size="${DEFAULT_FONT_SIZE}"
 declare param_latest=""
 declare param_debug=""
 declare param_no_backmatter=""
@@ -103,28 +124,23 @@ declare param_paper_size=""
 while [ $# -gt 0 ]; do
     case "${1}" in
         --debug)            param_debug="--debug" ; shift ;;
+        --font-size)        param_font_size="--font-size ${2}" ; shift 2 ;;
         --engine)           arg_engine="${2,,}" ; shift 2 ;;
-        --use-latest)       param_latest="--use-latest" ; shift ;;
         --latex)            param_latex="--latex" ; shift ;;
         --latex-only)       param_latex_only="--latex-only" ; shift ;;
         --markdown)         param_markdown_file="${2}" ; shift 2 ;;
         --no-image)         param_no_image="--no-image" ; shift ;;
         --no-frontmatter)   param_no_frontmatter="--no-frontmatter" ; shift ;;
         --no-backmatter)    param_no_backmatter="--no-backmatter" ; shift ;;
-        --paper)            param_paper_size="--paper ${2}" ; shift ;;
+        --paper-size)       param_paper_size="--paper-size ${2}" ; shift 2 ;;
         --show-frame)       param_show_frame="--show-frame" ; shift ;;
         --template)         arg_template="${2}" ; shift 2 ;;
+        --use-latest)       param_latest="--use-latest" ; shift ;;
         *)                  break ;;
     esac
 done
 
 
-# if [[ -n "${arg_paper_size}" ]]; then
-#     param_paper_size="--paper ${arg_paper_size}"
-# fi
-
-echo "******************************************"
-echo "${param_paper_size}"
 
 if [ ! -f "${param_markdown_file}" ]; then
     echo "Error: Input markdown content file not found: ${param_markdown_file}"
@@ -133,6 +149,7 @@ fi
 ${DOC_BIN}                                      \
     ${param_latest}                             \
     ${param_debug}                              \
+    ${param_font_size}                          \
     ${param_paper_size}                         \
     ${param_show_frame}                         \
     ${param_no_image}                           \
